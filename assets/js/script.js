487d85b89a7780c5857962670b6d587ad496dcce
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-04-03 22:15:21 +0100

    added fetch data function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,44 @@
 const renderCities = () => {};
 
-const fetchAllWeatherData = () => {};
+const fetchAllWeatherData = (cityName) => {
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a6cdce351d249a3594ef62adb60dd561`;
+
+  const functionForJSON = (responseObject) => responseObject.json();
+
+  const functionForApplication = (dataFromServer) => {
+    const lat = dataFromServer.coord.lat;
+    const lon = dataFromServer.coord.lon;
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=a6cdce351d249a3594ef62adb60dd561`;
+
+    const functionForJSON = (responseObject) => responseObject.json();
+
+    const functionForApplication = (dataFromServer) => {
+      // whatever your application code is goes here
+      console.log(dataFromServer);
+      // call a function getCurrentData() to get data from dataFromServer
+      // getCurrentData() and store in currentData
+      // getForecastData() and store in foreCastData
+      // renderCurrentCardComponent(currentData)
+      // renderForecastCardComponent(forecastData)
+    };
+
+    const functionToHandleError = (errorObject) => {
+      // handle your error here according to your application
+    };
+
+    fetch(url)
+      .then(functionForJSON)
+      .then(functionForApplication)
+      .catch(functionToHandleError);
+  };
+
+  const functionToHandleError = (errorObject) => {};
+
+  fetch(url)
+    .then(functionForJSON)
+    .then(functionForApplication)
+    .catch(functionToHandleError);
+};
 
 const onLoad = () => {
   // read from local storage and store data in variable called citiesFromLocalStorage
@@ -15,7 +53,7 @@ const onLoad = () => {
   const length = citiesFromLocalStorage.length;
   const lastCity = citiesFromLocalStorage[length];
 
-  fetchAllWeatherData(lastCity);
+  fetchAllWeatherData("Birmingham");
 };
 
 $(document).ready(onLoad);
